ca4d33908747685ac48adbf721984449af60159c
bumslap <bumslap@naver.com>
2018-12-04 00:55:07 +0900

    Merge branch 'master' of https://github.com/HoneyPot8014/AbroadApp
    # Conflicts:
    #	app/src/main/java/com/example/leeyh/abroadapp/view/fragment/LocationFragment.java

--- app/src/main/java/com/example/leeyh/abroadapp/view/activity/MemberDetailViewActivity.java
+++ app/src/main/java/com/example/leeyh/abroadapp/view/activity/MemberDetailViewActivity.java
@@ -1,6 +1,8 @@
 package com.example.leeyh.abroadapp.view.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,14 +14,26 @@ import android.os.Bundle;
          import android.support.v7.app.AppCompatActivity;
          import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.leeyh.abroadapp.helper.ApplicationManagement;
 import com.google.android.gms.maps.CameraUpdateFactory;
          import com.google.android.gms.maps.GoogleMap;
          import com.google.android.gms.maps.MapView;
          import com.google.android.gms.maps.OnMapReadyCallback;
          import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import static com.example.leeyh.abroadapp.constants.SocketEvent.MAKE_CHAT_ROOM;
+import static com.example.leeyh.abroadapp.constants.StaticString.USER_INFO;
+import static com.example.leeyh.abroadapp.constants.StaticString.USER_UUID;
 
 public class MemberDetailViewActivity extends AppCompatActivity implements OnMapReadyCallback{
     //implements OnMapReadyCallback
@@ -28,30 +42,41 @@ public class MemberDetailViewActivity extends AppCompatActivity implements OnMap
     private String UUID;
     private ImageView profileImg;
     private static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyD3q4iAopnm9lr3CHbGXpfBFfnhBAY4w2c";
+    private Button messageButton;
+    private SharedPreferences mSharedPreferences;
+    private ApplicationManagement mAppManager;
+    private TextView nameAndAgeTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_detail_view);
-
-
-
+        nameAndAgeTextView = findViewById(R.id.nameAndAgeTextView);
+        messageButton = findViewById(R.id.messageButton);
+        mSharedPreferences = getSharedPreferences(USER_INFO, Context.MODE_PRIVATE);
+        mAppManager = (ApplicationManagement) getApplicationContext();
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
         }
 
         mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
-       // mapView.getMapAsync(this);
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONArray makeRoomIdsData = new JSONArray();
 
+                    makeRoomIdsData.put(mSharedPreferences.getString(USER_UUID, null));
+                    makeRoomIdsData.put(UUID);
 
+                    mAppManager.emitRequestToServer(MAKE_CHAT_ROOM, makeRoomIdsData);
 
-        // inside your activity (if you did not enable transitions in your theme)
-       // getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-
-// set an exit transition
-        //getWindow().setExitTransition(new Explode());
+            }
+        });
     }
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -68,20 +93,21 @@ public class MemberDetailViewActivity extends AppCompatActivity implements OnMap
     @Override
     protected void onResume() {
         super.onResume();
-      //  mapView.onResume();
+       mapView.onResume();
         Intent intent = getIntent();
         UUID = intent.getStringExtra("name");
         profileImg = findViewById(R.id.profileImg);
+        nameAndAgeTextView.setText(mSharedPreferences+", 25(M)");
         Glide.with(getApplicationContext()).load("http://49.236.137.55/profile?id=" + UUID + ".jpeg").into(profileImg);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //mapView.onCreate(mapViewBundle);
-        mapView.getMapAsync(this);
+        mapView.onStart();
+
     }
-/*
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -101,7 +127,7 @@ public class MemberDetailViewActivity extends AppCompatActivity implements OnMap
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
-    }*/
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
